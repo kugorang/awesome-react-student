d48bfa3205e4e778832de008b0c7603ca9556e4b
Kim Hyeonwoo <1reds@naver.com>
2019-03-23 12:37:33 +0900

    Add Webpack Config Folder
    config 폴더 생성 후 react build 진행함

---
+++ 6week/youflix/build/precache-manifest.1808b3404d46cddb57ac092394909f00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4829d0e1d611e7043686",
+    "url": "/static/js/main.0e3c6501.chunk.js"
+  },
+  {
+    "revision": "a45f741e361ab42efe3b",
+    "url": "/static/js/2.557b0073.chunk.js"
+  },
+  {
+    "revision": "4829d0e1d611e7043686",
+    "url": "/static/css/main.d75cae8f.chunk.css"
+  },
+  {
+    "revision": "a45f741e361ab42efe3b",
+    "url": "/static/css/2.2336687c.chunk.css"
+  },
+  {
+    "revision": "42f7a83c4d3fbd27e13497575718528e",
+    "url": "/index.html"
+  }
+];
